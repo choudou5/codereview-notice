fa7ec1a52fc6e7b6f8c7dbeee05e248f921c8252
choudou5 <xuhaowende@sina.cn>
2017-03-20 03:08:54 +0800

    OSS上传

--- src/main/java/com/dreamerpartner/codereview/util/OSSClientUtil.java
+++ src/main/java/com/dreamerpartner/codereview/util/OSSClientUtil.java
@@ -1,6 +1,11 @@
 package com.dreamerpartner.codereview.util;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+
+import javax.servlet.ServletException;
+
+import org.apache.commons.lang.StringUtils;
 
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.model.PutObjectResult;
@@ -14,23 +19,41 @@ public class OSSClientUtil {
 
 	private static OSSClient client = null;
 	private static String bucketName;
+	private static String uploadDir;
+	private static String ossPath;
 	
-	public static void init(){
-		String endpoint = "http://choudoufu-hd2.oss-cn-shanghai.aliyuncs.com";
-		// accessKey请登录https://ak-console.aliyun.com/#/查看
-		String accessKeyId = PropertiesUtil.getString("oss.accessKeyId");
-		String accessKeySecret = PropertiesUtil.getString("oss.accessSecret");
-		bucketName = PropertiesUtil.getString("oss.bucketName");
-		client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+	
+	
+	public static void init() throws ServletException{
+		if(StringUtils.isBlank(bucketName)){
+			String endpoint = "http://oss-cn-shanghai.aliyuncs.com";
+			// accessKey请登录https://ak-console.aliyun.com/#/查看
+			String accessKeyId = PropertiesUtil.getString("oss.accessKeyId");
+			String accessKeySecret = PropertiesUtil.getString("oss.accessSecret");
+			bucketName = PropertiesUtil.getString("oss.bucketName");
+			uploadDir = PropertiesUtil.getString("oss.upload.dir");
+			ossPath = PropertiesUtil.getString("oss.path");
+			client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+			boolean exists = client.doesBucketExist(bucketName);
+			if(!exists){
+				throw new ServletException("初始化 OSS失败. bucketName:"+bucketName+" 不存在.");
+			}
+			//创建目录
+			client.putObject(bucketName, uploadDir, new ByteArrayInputStream(new byte[0]));
+		}
 	}
 	
 	/**
 	 * 上传图片
+	 * @param pathKey
 	 * @param is
 	 */
-	public static void uploadImage(InputStream is){
-		PutObjectResult result = client.putObject(bucketName, "image", is);
-		System.out.println(JsonUtil.toString(result));
+	public static PutObjectResult uploadImage(String pathKey, InputStream is){
+		return client.putObject(bucketName, uploadDir+pathKey, is);
+	}
+	
+	public static String getOssPath(){
+		return ossPath+uploadDir;
 	}
 	
 	
